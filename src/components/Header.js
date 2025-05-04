8d3cebb129c49262b9aedcfc3c0defe00b151a5f
Daniel Luke Rogers <dluke.rogers@gmail.com>
2025-05-04 00:04:37 -0500

    Added link to devicons, replaced laptop image with logo, changed description

--- src/components/Header.js
+++ src/components/Header.js
@@ -94,8 +94,7 @@ class Header extends Component {
         <div className="row aligner" style={{ height: '100%' }}>
           <div className="col-md-12">
             <div>
-              <img src="./public/images/portfolio-logo.svg" alt="LR logo"></img>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+              <img src="/images/portfolio-logo.svg" alt="LR logo"></img>
               <br />
               <h1 className="mb-0">
                 {name}
